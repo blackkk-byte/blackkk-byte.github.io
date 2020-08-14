2cfc755e4029fc3be57e414a2387f807445a9e41
blackkk-byte <LXkltelksf@protonmail.com>
2020-08-14 10:19:33 -0700

    Site updated: 2020-08-14 10:19:33

---
+++ js/not.js
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="https://api.myip.com/"></script> 
+<script type="text/javascript">
+if (returnCitySN.cname.indexOf('USA')==-1){
+window.location.href='www.github.com'
+}
+</script>
